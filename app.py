4a36c44847b20a23d9a39761be731a70cd162589
ty875 <tingyue0313@gmail.com>
2025-02-02 15:52:47 -0500

    predict route

---
+++ app.py
@@ -0,0 +1,31 @@
+def index():
+    if request.method == 'POST':
+        try:
+            #  reading the inputs given by the user
+            fixed_acidity =float(request.form['fixed_acidity'])
+            volatile_acidity =float(request.form['volatile_acidity'])
+            citric_acid =float(request.form['citric_acid'])
+            residual_sugar =float(request.form['residual_sugar'])
+            chlorides =float(request.form['chlorides'])
+            free_sulfur_dioxide =float(request.form['free_sulfur_dioxide'])
+            total_sulfur_dioxide =float(request.form['total_sulfur_dioxide'])
+            density =float(request.form['density'])
+            pH =float(request.form['pH'])
+            sulphates =float(request.form['sulphates'])
+            alcohol =float(request.form['alcohol'])
+       
+         
+            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
+            data = np.array(data).reshape(1, 11)
+            
+            obj = PredictionPipeline()
+            predict = obj.predict(data)
+
+            return render_template('results.html', prediction = str(predict))
+
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+
+    else:
+        return render_template('index.html')
